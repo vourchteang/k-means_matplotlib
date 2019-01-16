9a6f6a76c42e8e6f79886163570480b4a839fb7b
Vourchteang Sea <vourchteangs@gmail.com>
2019-01-16 19:09:45 +0900

    Create kmeans.py
    This code aims at helping people understand the process of k-means clustering algorithm step-by-step.

---
+++ kmeans.py
@@ -0,0 +1,109 @@
+### Import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+### Initial Stage
+
+df = pd.DataFrame({
+    'x': [12, 20, 28, 18, 29, 33, 24, 45, 45, 52, 51, 52, 55, 53, 55, 61, 64, 69, 72],
+    'y': [39, 36, 30, 52, 54, 46, 55, 59, 63, 70, 66, 63, 58, 23, 14, 8, 19, 7, 24]
+})
+
+np.random.seed(300)
+k = 3
+centroids = {i + 1: [np.random.randint(0, 80), np.random.randint(0, 80)] for i in range(k)} # centroids[i] = [x, y]
+
+fig = plt.figure(figsize=(5, 5))
+plt.scatter(df['x'], df['y'], color='k')
+colors = {1: 'b', 2: 'r', 3: 'g'}
+for i in centroids.keys():
+    plt.scatter(*centroids[i], color=colors[i])
+plt.xlim(0, 80)
+plt.ylim(0, 80)
+
+### Assignment Stage
+
+def assignment(df, centroids):
+    for i in centroids.keys():
+        # sqrt((x1 - x2)^2 - (y1 - y2)^2)
+        df['distance_from_{}'.format(i)] = (
+            np.sqrt(
+                (df['x'] - centroids[i][0]) ** 2
+                + (df['y'] - centroids[i][1]) ** 2
+            )
+        )
+    centroid_distance_cols = ['distance_from_{}'.format(i) for i in centroids.keys()]
+    df['closest'] = df.loc[:, centroid_distance_cols].idxmin(axis=1)
+    df['closest'] = df['closest'].map(lambda x: int(x.lstrip('distance_from_')))
+    df['color'] = df['closest'].map(lambda x: colors[x])
+    return df
+
+
+df = assignment(df, centroids)
+
+fig = plt.figure(figsize=(5, 5))
+plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
+for i in centroids.keys():
+    plt.scatter(*centroids[i], color=colors[i])
+plt.xlim(0, 80)
+plt.ylim(0, 80)
+
+
+### Update Stage
+
+import copy
+
+old_centroids = copy.deepcopy(centroids)
+
+def update(k):
+    for i in centroids.keys():
+        centroids[i][0] = np.mean(df[df['closest'] == i]['x'])
+        centroids[i][1] = np.mean(df[df['closest'] == i]['y'])
+    return k
+
+centroids = update(centroids)
+
+fig = plt.figure(figsize=(5, 5))
+ax = plt.axes()
+plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
+for i in centroids.keys():
+    plt.scatter(*centroids[i], color=colors[i])
+plt.xlim(0, 80)
+plt.ylim(0, 80)
+for i in old_centroids.keys():
+    old_x = old_centroids[i][0]
+    old_y = old_centroids[i][1]
+    dx = (centroids[i][0] - old_centroids[i][0]) * 0.75
+    dy = (centroids[i][1] - old_centroids[i][1]) * 0.75
+    ax.arrow(old_x, old_y, dx, dy, head_width=2, head_length=3, fc=colors[i], ec=colors[i])
+
+
+### Repeat Assigment Stage
+
+df = assignment(df, centroids)
+
+# Plot results
+fig = plt.figure(figsize=(5, 5))
+plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
+for i in centroids.keys():
+    plt.scatter(*centroids[i], color=colors[i])
+plt.xlim(0, 80)
+plt.ylim(0, 80)
+
+### Continue until all assigned centroids no longer move
+
+while True:
+    closest_centroids = df['closest'].copy(deep=True)
+    centroids = update(centroids)
+    df = assignment(df, centroids)
+    if closest_centroids.equals(df['closest']):
+        break
+
+fig = plt.figure(figsize=(5, 5))
+plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
+for i in centroids.keys():
+    plt.scatter(*centroids[i], color=colors[i])
+plt.xlim(0, 80)
+plt.ylim(0, 80)
+plt.show()
